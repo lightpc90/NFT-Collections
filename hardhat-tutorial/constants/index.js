6bef074629dac86c5d93238e7af6bd7d6b8fdb80
lightpc90 <afoageektech@gmail.com>
2023-02-04 21:02:21 +0100

    redeployed with a new metadataurl Using my vercel domain And creating an API endpoint

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x7E6E6353C1AE7fa06724Ff6DF94099c58e89f4C0";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "nft-collections-a3jv1g6dk-lightpc90.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
